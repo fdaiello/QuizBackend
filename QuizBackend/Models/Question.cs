5616ce02cd9be665dd70583aaf671c9eda0e6de2
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-05-26 20:34:09 -0300

    multiple answers

--- QuizBackend/Models/Question.cs
+++ QuizBackend/Models/Question.cs
@@ -2,6 +2,13 @@
 {
     public class Question
     {
+        public Question ( string text, string answer1, string answer2, string answer3)
+        {
+            _text = text;
+            Answer1 = answer1;
+            Answer2 = answer2;
+            Answer3 = answer3;
+        }
         private string _text = String.Empty;
         
         private int _id;
@@ -31,5 +38,9 @@
                 }
             }
         }
+
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
     }
 }
