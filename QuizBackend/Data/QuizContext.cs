e7f6758c88bac3a218368a4032dbad44de085a55
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-05-26 11:47:23 -0300

    Add project files.

---
+++ QuizBackend/Data/QuizContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizBackend.Models;
+
+namespace QuizBackend.Data
+{
+    public class QuizContext : DbContext
+    {
+        public QuizContext(DbContextOptions<QuizContext> options) : base(options) { }
+
+        public DbSet<Question> Questions { get; set; }
+
+    }
+}
---
+++ QuizBackend/Models/Question.cs
@@ -0,0 +1,35 @@
+﻿namespace QuizBackend.Models
+{
+    public class Question
+    {
+        private string _text = String.Empty;
+        
+        private int _id;
+
+        public int Id { 
+            get => _id;
+            set
+            {
+                if (value > 0)
+                    _id = value;
+                else
+                    throw new ArgumentException("Id must be greater than zero");
+            }
+        }
+
+        public string Text { 
+            get => _text;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _text = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Text cannot be null nor empty.");
+                }
+            }
+        }
+    }
+}
